fde30bc43ed50d28ea0639e12ee46e59a920395e
Azzamok <azzamok@hotmail.com>
2015-02-15 04:00:47 +1100

    Defined a constant.

--- PlayingWithC/Hello.c
+++ PlayingWithC/Hello.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define MYCONSTANT "This cannot ever be changed!"
 
 //Prints "Hello World", and other stuff...
 
@@ -9,8 +11,13 @@ int main()
 	printf("Hello world \n");
 	printf("%d \n", blah);
 
-	char alist[6] = "Potato"; //Creating an array of 6 bytes
-	printf("I like %s", alist);
+	char alist[6] = "potato"; //Creating an array of 6 bytes
+	printf("I like %s \n", alist);
+	strcpy(alist, "sushi"); //Replaces potato with sushi
+	printf("I like %s \n", alist);
+
+	printf("Can a constant be changed? %s", MYCONSTANT); //Uses the predefined constant string
+
 
 	return 0;
 }
