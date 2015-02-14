1f7c1b3001102d6a76b53623cc484a1a281ade6a
Azzamok <azzamok@hotmail.com>
2015-02-15 03:39:33 +1100

    Utilised '%'. Defined variables. Created an array.

--- PlayingWithC/Hello.c
+++ PlayingWithC/Hello.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Prints "Hello World"
+//Prints "Hello World", and other stuff...
 
 int main()
 {
-	printf("Hello world");
+	int blah = 20; //Defining variables
+	printf("Hello world \n");
+	printf("%d \n", blah);
+
+	char alist[6] = "Potato"; //Creating an array of 6 bytes
+	printf("I like %s", alist);
+
 	return 0;
 }
