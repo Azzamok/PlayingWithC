0cfb5c57ee88440533cc774bcdbf4eb8971c6d11
Azzamok <azzamok@hotmail.com>
2015-02-15 04:32:34 +1100

    Added a prompt. Disabled buffer.

--- PlayingWithC/Hello.c
+++ PlayingWithC/Hello.c
@@ -7,6 +7,8 @@
 
 int main()
 {
+	setbuf(stdout, NULL); //Disabled the buffer
+
 	int blah = 20; //Defining variables
 	printf("Hello world \n");
 	printf("%d \n", blah);
@@ -16,8 +18,12 @@ int main()
 	strcpy(alist, "sushi"); //Replaces potato with sushi
 	printf("I like %s \n", alist);
 
-	printf("Can a constant be changed? %s", MYCONSTANT); //Uses the predefined constant string
+	printf("Can a constant be changed? %s \n", MYCONSTANT); //Uses the predefined constant string
 
+	char user[20];
+	printf("Enter a username... \n");
+	scanf("%s", user);
+	printf("Hello there, %s", user);
 
 	return 0;
 }
